8c8eb9a972bba9017945f7053b8c40ff9ceb612b
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-28 17:11:46 +0300

    Single task view
    Add a link to view single task

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import  CreateView
 from .models import Task
 
 # Create your views here.
@@ -11,3 +12,5 @@ class TaskList(ListView):
     
 class TaskDetail(DetailView):
     model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
