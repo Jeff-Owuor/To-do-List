0e7288a5e5ca4e62beaaeb09dfc9a116bd0fd881
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-28 15:42:10 +0300

    Register the views
    Import each of the two views from the views file
    Add a path with ListView
    Add a path with the DetailView

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title','description','created','complete')
+    search_fields = ('title',)
+    admin.site.site_header = 'To-do List'
+    
+    
 
 # Register your models here.
+admin.site.register(Task,TaskAdmin)
