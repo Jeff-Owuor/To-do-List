77f7a1a8e683d4e93e5a4e124a7ab32539fa536a
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-29 10:47:36 +0300

    Search functionality
    Create a search form in html
    Set name to search-area
    add a submit button
    Make a get request to receive data containing the searched input

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import  CreateView,UpdateView,DeleteView,FormView
@@ -26,10 +26,15 @@ class RegisterPage(FormView):
     success_url = reverse_lazy('tasks')
     
     def form_valid(self,form):
-        user = form.save()
-        if user is not None:
+        user = form.save() # get the registered user
+        if user is not None:# log the registered user in 
             login(self.request,user)
         return super(RegisterPage,self).form_valid(form)
+    
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage,self).get(*args,**kwargs)
 
 class TaskList(LoginRequiredMixin,ListView):
     model = Task
@@ -40,6 +45,11 @@ class TaskList(LoginRequiredMixin,ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user= self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontians = search_input)
+        context['search_input'] = search_input
         return context
     
 class TaskDetail(LoginRequiredMixin,DetailView):
